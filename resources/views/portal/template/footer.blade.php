01b42f47bc700a0eb5ce748af3d1ff2b0e1439e3
Allan Ramos <allangrds@gmail.com>
2019-01-25 14:51:48 -0200

    feat(Project): Add portal footer template

--- resources/views/portal/template/footer.blade.php
+++ resources/views/portal/template/footer.blade.php
@@ -1,1 +1,61 @@
-Rodapé
+<footer class="footer">
+    <div class="columns">
+        <div class="column">
+            <img
+                alt="IASD Belém - Logo"
+                src="/images/footer/logo.png"
+            />
+            <span class="title">Sobre nós</span>
+            <p class="text about">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                hendrerit pharetra justo id bibendum. Ut ornare felis a mauris
+                elementum, sed aliquet erat mollis.
+            </p>
+        </div>
+        <div class="column">
+            <div class="columns">
+                <div class="column">
+                    <span class="title">Nosso horários</span>
+                    <ul class="time-list">
+                        <li class="time-item">Sábado | 9:30</li>
+                        <li class="time-item">Domingo | 19:00</li>
+                        <li class="time-item">Quarta | 19:00</li>
+                    </ul>
+                </div>
+                <div class="column">
+                    <span class="title">Onde estamos</span>
+                    <p class="text">
+                        Rua Martim Affonso, 152 - Belenzinho, São Paulo - SP, 03057-050
+                    </p>
+                </div>
+            </div>
+            <div class="columns">
+                <div class="column">
+                    <span class="title">Redes sociais</span>
+                    <a class="social-network" href="">
+                        <img
+                            src="/images/footer/facebook.png"
+                            alt="Nosso facebook"
+                        />
+                    </a>
+                    <a class="social-network" href="">
+                        <img
+                            src="/images/footer/instagram.png"
+                            alt="Nosso instagram"
+                        />
+                    </a>
+                    <a class="social-network" href="">
+                        <img
+                            src="/images/footer/twitter.png"
+                            alt="Nosso twitter"
+                        />
+                    </a>
+                </div>
+                <div class="column">
+                    <span class="title">Fale conosco</span>
+                    <p class="text">Email: contato@iasdbelem.com.br</p>
+                </div>
+            </div
+        </div>
+    </div>
+</footer>
