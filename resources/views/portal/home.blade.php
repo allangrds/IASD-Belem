eb073c0308489409106e0f75ebd1b061bb65674b
Allan Ramos <allangrds@gmail.com>
2019-01-25 17:34:19 -0200

    feat(Home): Add featured cards

--- resources/views/portal/home.blade.php
+++ resources/views/portal/home.blade.php
@@ -3,7 +3,54 @@
 @section('content')
     @include('portal.template.header')
 
-    Conteúdo
+    <section class="container featured">
+        <div class="columns">
+            <div class="column floated-box is-3-desktop is-3-tablet">
+                <div class="content">
+                    <img
+                        class="icon"
+                        src="/images/home/culto.png"
+                        alt="Computar com botão de play"
+                    />
+                    <h1 class="title">Culto ao vivo</h1>
+                    <h2 class="subtitle">Veja nosso culto da sua casa</h2>
+                    <a class="button is-link" href="">
+                        Acompanhe agora
+                    </a>
+                </div>
+            </div>
+
+            <div class="column floated-box is-3-desktop is-3-tablet">
+                <div class="content">
+                    <img
+                        class="icon"
+                        src="/images/home/informativo.png"
+                        alt="Um folder dobrado"
+                    />
+                    <h1 class="title">Informativo</h1>
+                    <h2 class="subtitle">Veja a programação de 20/12</h2>
+                    <a class="button is-link" href="">
+                        Ler agora
+                    </a>
+                </div>
+            </div>
+
+            <div class="column floated-box is-3-desktop is-3-tablet">
+                <div class="content">
+                    <img
+                        class="icon"
+                        src="/images/home/lideranca.png"
+                        alt="Um grupo de pessoas"
+                    />
+                    <h1 class="title">Liderança</h1>
+                    <h2 class="subtitle">Conheça nossos líderes</h2>
+                    <a class="button is-link" href="">
+                        Ver informações
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
 
     @include('portal.template.footer')
 @endsection
