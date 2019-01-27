6ef0313f2c39b2c8eaad241d41805d0248d9d1ba
Allan Ramos <allangrds@gmail.com>
2019-01-27 13:14:27 -0200

    feat(Contact): Add contact page

---
+++ resources/views/portal/contato.blade.php
@@ -0,0 +1,67 @@
+@extends('base')
+
+@section('content')
+    @include('portal.template.header')
+
+    <div class="contact">
+        <div class="header container">
+            <h1 class="title">Fale conosco</h1>
+        </div>
+
+        <div class="content">
+            <div class="container">
+                <div class="columns">
+                    <div class="column is-8">
+                        <form action="">
+                            <div class="columns">
+                                <div class="column">
+                                    <input
+                                        aria-label="name"
+                                        class="input"
+                                        name="name"
+                                        placeholder="Digite seu nome"
+                                        type="text"
+                                    />
+                                </div>
+                                <div class="column">
+                                    <input
+                                        aria-label="email"
+                                        class="input"
+                                        name="email"
+                                        placeholder="Digite seu email"
+                                        type="text"
+                                    />
+                                </div>
+                            </div>
+                            <div class="columns">
+                                <div class="column">
+                                    <textarea
+                                        aria-label="message"
+                                        class="textarea"
+                                        name="message"
+                                        placeholder="Digite sua mensagem"
+                                    ></textarea>
+                                </div>
+                            </div>
+                            <input
+                                aria-label="Enviar mensagem"
+                                class="button is-primary"
+                                value="Enviar mensagem"
+                                type="submit"
+                            />
+                        </form>
+                    </div>
+                    <div class="address column is-4">
+                        <span class="title">Endereço</span>
+                        <span class="text">
+                            Rua Martim Affonso, 152 - Belenzinho,
+                            São Paulo - SP, 03057-050
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @include('portal.template.footer')
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,7 @@
 Route::get('/', function () {
     return view('portal.home');
 })->name('portal_home');
+
+Route::get('/fale-conosco', function () {
+    return view('portal.contato');
+})->name('portal_contato');
