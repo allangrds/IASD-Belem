1ce77098cb87b934b385605f2bd8737570b07c1d
Allan Ramos <allangrds@gmail.com>
2019-02-03 17:52:03 -0200

    feat(Errors): Add errors pages

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,9 @@
+@extends('errors.base')
+
+@section('status_code')
+    403
+@endsection
+
+@section('message')
+    Você não tem permissão para fazer isso :(
+@endsection
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+@extends('errors.base')
+
+@section('status_code')
+    404
+@endsection
+
+@section('message')
+    Essa página não existe :(
+@endsection
